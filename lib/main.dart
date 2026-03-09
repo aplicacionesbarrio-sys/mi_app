7e2632a11aa9e40778de3000602549d6d625667e
Aplicaciones Barrio <aplicaciones.barrio@gmail.com>
2026-03-09 19:33:07 -0300

    firebase y firestone funcionando

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
